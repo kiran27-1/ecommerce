faa10207e5e36737ebd47889311f3a73216e4393
kiran27-1 <96146372+kiran27-1@users.noreply.github.com>
2024-08-30 22:35:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Smooth Scroll for Anchor Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
